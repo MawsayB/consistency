a53d41713fc8d8a85da66755147fa0a8b880483c
Marcy Bursac <msbursac@gmail.com>
2017-11-01 16:10:14 -0500

    built skeletons for pages

--- main.py
+++ main.py
@@ -3,19 +3,32 @@ from flask import Flask, request, redirect, render_template, session, flash
 import cgi
 
 app = Flask(__name__)
-#app.config['DEBUG'] = True   
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:mbadded@localhost:8889/blogz'
+app.config['DEBUG'] = True   
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://consistent:fitness@localhost:8889/consistent'
 #app.config['SQLALCHEMY_ECHO'] = True
 #db = SQLAlchemy(app)
 #app.secret_key = 'mawsaysFinalProject'
 
 #class User(db.Model):
+    #id
+    #username
+    #password
+
+#class Exercise(db.Model):
+    #id - num
+    #name of exercise - string
+    #lbs (100's, 10's, 1's place on individual dials)
+    #equipment
+    #setting (chair, bench)
+
+#class Progress(db.Model):
+    #date
+    #user
+    #name of exercise
+    #equipment
+    #lbs
 
 @app.route('/', methods=['POST', 'GET'])
-def index(): 
-    return render_template('home.html')
-
-@app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
@@ -31,6 +44,14 @@ def login():
             flash('User password incorrect, or user does not exist', 'error')
 
     return render_template('login.html')
+
+@app.route('/home', methods=['POST', 'GET'])
+def home():
+    return render_template('home.html')
+
+@app.route('/lift', methods=['POST', 'GET'])
+def lift():
+    return render_template('lift.html')
 
 if __name__ == '__main__':
     app.run()
